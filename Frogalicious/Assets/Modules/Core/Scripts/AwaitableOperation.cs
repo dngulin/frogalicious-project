79a28933f17e8b9a0d53d5f8f527e2d29ac6a126
Danil Gulin <d.nk.gulin@gmail.com>
2024-04-22 22:31:02 +0200

    Add non-generic AwaitableOperation

--- Frogalicious/Assets/Modules/Core/Scripts/AwaitableOperation.cs
+++ Frogalicious/Assets/Modules/Core/Scripts/AwaitableOperation.cs
@@ -18,7 +18,7 @@ namespace Frog.Core
             return _acs.Awaitable;
         }
 
-        public bool TrySetResult(T result)
+        public bool TryEnd(T result)
         {
             if (!TryUnregisterCancellation())
                 return false;
@@ -55,12 +55,69 @@ namespace Frog.Core
         }
     }
 
+    public class AwaitableOperation : IDisposable
+    {
+        private readonly AwaitableCompletionSource _acs = new AwaitableCompletionSource();
+        private CancellationTokenRegistration? _ctr;
+
+        public Awaitable ExecuteAsync(CancellationToken ct)
+        {
+            TryCancel();
+            RegisterCancellation(ct);
+
+            _acs.Reset();
+            return _acs.Awaitable;
+        }
+
+        public bool TryEnd()
+        {
+            if (!TryUnregisterCancellation())
+                return false;
+
+            _acs.SetResult();
+            return true;
+        }
+
+        public bool TryCancel()
+        {
+            if (!TryUnregisterCancellation())
+                return false;
+
+            _acs.SetCanceled();
+            return true;
+        }
+
+        public void Dispose() => TryCancel();
+
+        private void RegisterCancellation(CancellationToken ct)
+        {
+            Debug.Assert(_ctr == null, "CancellationTokenRegistration will be overwritten!");
+            _ctr = ct.Register(() => TryCancel());
+        }
+
+        private bool TryUnregisterCancellation()
+        {
+            if (_ctr == null)
+                return false;
+
+            _ctr.Value.Dispose();
+            _ctr = null;
+            return true;
+        }
+    }
+
     public static class AwaitableProcessExtensions
     {
         public static void EndAssertive<T>(this AwaitableOperation<T> proc, T result)
         {
-            var ended = proc.TrySetResult(result);
-            Debug.Assert(ended, $"Failed to end the process with result {result}. Process is not being awaited!");
+            var ended = proc.TryEnd(result);
+            Debug.Assert(ended, $"Failed to end an operation with result {result}. It is not being awaited!");
+        }
+
+        public static void EndAssertive(this AwaitableOperation proc)
+        {
+            var ended = proc.TryEnd();
+            Debug.Assert(ended, $"Failed to end an operation. It is not being awaited!");
         }
     }
 }
