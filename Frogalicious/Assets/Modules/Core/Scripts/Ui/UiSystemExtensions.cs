1e28686966fdbc2bf07b4a8628cda373f41c60a8
Danil Gulin <d.nk.gulin@gmail.com>
2024-05-04 20:54:07 +0200

    Fix ui holders naming

--- Frogalicious/Assets/Modules/Core/Scripts/Ui/UiSystemExtensions.cs
+++ Frogalicious/Assets/Modules/Core/Scripts/Ui/UiSystemExtensions.cs
@@ -6,34 +6,34 @@ namespace Frog.Core.Ui
 {
     public static class UiSystemExtensions
     {
-        public static FullScreenContainerHolder FullscreenUi<T>(this UiSystem ui, T contents) where T : MonoBehaviour
+        public static FullScreenUiHolder FullscreenUi<T>(this UiSystem ui, T contents) where T : MonoBehaviour
         {
             var parent = contents.transform.parent;
             var id = ui.ShowFullscreen(contents.transform);
-            return new FullScreenContainerHolder(ui, id, parent);
+            return new FullScreenUiHolder(ui, id, parent);
         }
 
-        public static LoadingWindowHolder LoadingUi(this UiSystem ui)
+        public static LoadingUiHolder LoadingUi(this UiSystem ui)
         {
             ui.ShowLoading();
-            return new LoadingWindowHolder(ui);
+            return new LoadingUiHolder(ui);
         }
 
-        public static async Awaitable<DynWindowHolder> AnimatedUi(this UiSystem ui, DynUiEntity entity, CancellationToken ct)
+        public static async Awaitable<DynUiEntityHolder> AnimatedUi(this UiSystem ui, DynUiEntity entity, CancellationToken ct)
         {
             var parent = entity.transform.parent;
             var id = await ui.Show(entity, ct);
-            return new DynWindowHolder(ui, id, parent, ct);
+            return new DynUiEntityHolder(ui, id, parent, ct);
         }
     }
 
-    public readonly struct FullScreenContainerHolder : IDisposable
+    public readonly struct FullScreenUiHolder : IDisposable
     {
         private readonly UiSystem _ui;
         private readonly FullScreenUiEntityId _id;
         private readonly Transform _contentsParent;
 
-        public FullScreenContainerHolder(UiSystem ui, FullScreenUiEntityId id, Transform contentsParent)
+        public FullScreenUiHolder(UiSystem ui, FullScreenUiEntityId id, Transform contentsParent)
         {
             _ui = ui;
             _id = id;
@@ -47,10 +47,10 @@ namespace Frog.Core.Ui
         }
     }
 
-    public readonly struct LoadingWindowHolder : IDisposable
+    public readonly struct LoadingUiHolder : IDisposable
     {
         private readonly UiSystem _ui;
-        public LoadingWindowHolder(UiSystem ui) => _ui = ui;
+        public LoadingUiHolder(UiSystem ui) => _ui = ui;
         public void Dispose()
         {
             if (_ui.IsUnderlyingGameObjectAlive)
@@ -58,14 +58,14 @@ namespace Frog.Core.Ui
         }
     }
 
-    public readonly struct DynWindowHolder
+    public readonly struct DynUiEntityHolder
     {
         private readonly UiSystem _ui;
         private readonly DynUiEntityId _id;
         private readonly Transform _parent;
         private readonly CancellationToken _ct;
 
-        public DynWindowHolder(UiSystem ui, DynUiEntityId id, Transform parent, CancellationToken ct)
+        public DynUiEntityHolder(UiSystem ui, DynUiEntityId id, Transform parent, CancellationToken ct)
         {
             _ui = ui;
             _id = id;
