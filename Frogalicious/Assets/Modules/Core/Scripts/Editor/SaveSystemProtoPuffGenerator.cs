79e4ee6245e580fe9cd64e3ee8cfbe7f901e6516
Danil Gulin <d.nk.gulin@gmail.com>
2025-03-28 23:05:22 +0100

    Inline codegen options

--- Frogalicious/Assets/Modules/Core/Scripts/Editor/SaveSystemProtoPuffGenerator.cs
+++ Frogalicious/Assets/Modules/Core/Scripts/Editor/SaveSystemProtoPuffGenerator.cs
@@ -8,17 +8,15 @@ namespace Frog.Core.Editor
         [MenuItem("Tools/Code Generation/Save System - Header", priority = 20)]
         public static void GenerateSaveInternal()
         {
-            var opts = new CodeGenOptions
-            {
-                Namespace = "Frog.Core.Save",
-                DeserialisationApi = ApiTargets.Stream,
-                SerialisationApi = ApiTargets.Stream,
-            };
-
             CodeGenerator.Generate(
                 "Assets/Modules/Core/Scripts/Save/SaveHeader.puff",
                 "Assets/Modules/Core/Scripts/Save/SaveHeader.g.cs",
-                opts
+                new CodeGenOptions
+                {
+                    Namespace = "Frog.Core.Save",
+                    DeserialisationApi = ApiTargets.Stream,
+                    SerialisationApi = ApiTargets.Stream,
+                }
             );
             AssetDatabase.Refresh();
         }
@@ -26,17 +24,15 @@ namespace Frog.Core.Editor
         [MenuItem("Tools/Code Generation/Save System - Data", priority = 21)]
         public static void GenerateSaveGame()
         {
-            var opts = new CodeGenOptions
-            {
-                Namespace = "Frog.Core.Save",
-                DeserialisationApi = ApiTargets.Stream,
-                SerialisationApi = ApiTargets.Stream,
-            };
-
             CodeGenerator.Generate(
                 "Assets/Modules/Core/Scripts/Save/FrogSave.puff",
                 "Assets/Modules/Core/Scripts/Save/FrogSave.g.cs",
-                opts
+                new CodeGenOptions
+                {
+                    Namespace = "Frog.Core.Save",
+                    DeserialisationApi = ApiTargets.Stream,
+                    SerialisationApi = ApiTargets.Stream,
+                }
             );
             AssetDatabase.Refresh();
         }
